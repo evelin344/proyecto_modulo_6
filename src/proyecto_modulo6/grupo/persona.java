2da78142a4e49783d78a487b9a2624c61f78d918
Evelin Orellana <evelin_orellana1215@hotmail.com>
2020-03-20 20:41:48 -0600

    cambios realizados en la clase persona Signed-off-by:Evelin Orellana <evelin_orellana1215@hotmail.com>

---
+++ src/proyecto_modulo6/grupo/persona.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_modulo6.grupo;
+
+/**
+ *
+ * @author Felix
+ */
+public abstract class persona{
+    public String nombre;
+    public String identidad;
+ 
+public persona(){
+
+} 
+public void establecerNombre(String nombre){
+    this.nombre=nombre;
+}
+public String obtenerNombre(){
+    return this.nombre;
+}
+public void imprimirNombre(){
+    System.out.println("nombre "+nombre);
+}
+  abstract String especialdad();
+  abstract String Disciplina();
+}
